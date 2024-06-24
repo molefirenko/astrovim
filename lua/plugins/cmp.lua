2681cc9e0a22a25ed11af7234b94adf152119788
TulakHord <tulakhord@webspark.com>
2024-06-24 22:19:09 +0300

    Changed cmp sources

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,8 +3,11 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.source = cmp.config.sources({
-        {name = "codeium"}
+      opts.sources = cmp.config.sources({
+        {name = "codeium", keyword_length = 3},
+        {name = "luasnip", keyword_length = 2},
+        {name = "nvim_lsp", keyword_length = 1},
+        {name = "buffer", keyword_length = 3},
       })
     end,
   }
